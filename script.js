57869984def115543a3d2f5cbb8eed91a843bdc3
Sanay-Pise <159645144+Sanay-Pise@users.noreply.github.com>
2024-09-19 22:59:32 +0530

    format documents

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 function calculateCalories() {
-    const gender = document.getElementById("gender").value;
-    const age = parseInt(document.getElementById("age").value);
-    const height = parseInt(document.getElementById("height").value);
-    const weight = parseInt(document.getElementById("weight").value);
-    
-    if (!gender || !age || !height || !weight) {
-        const resultElement = document.getElementById("result");
-        resultElement.innerHTML = `<p class="text-center mt-3">Please enter all required information!</p>`;
-        return;
-    }
+  const gender = document.getElementById("gender").value;
+  const age = parseInt(document.getElementById("age").value);
+  const height = parseInt(document.getElementById("height").value);
+  const weight = parseInt(document.getElementById("weight").value);
 
-    let bmr;
-
-    if (gender === "male") {
-        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
-    } else {
-        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
-    }
-
+  if (!gender || !age || !height || !weight) {
     const resultElement = document.getElementById("result");
-    resultElement.innerHTML = `<p class="text-center mt-3">Your estimated daily calorie needs: <strong>${bmr.toFixed(2)}</strong> calories</p>`;
+    resultElement.innerHTML = `<p class="text-center mt-3">Please enter all required information!</p>`;
+    return;
+  }
+
+  let bmr;
+
+  if (gender === "male") {
+    bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+  } else {
+    bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+  }
+
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = `<p class="text-center mt-3">Your estimated daily calorie needs: <strong>${bmr.toFixed(2)}</strong> calories</p>`;
 }
 
 const wrapper = document.querySelector('.wrapper');
 const loginlink = document.querySelector('.login-link');
 const registerlink = document.querySelector('.register-link');
 
-registerlink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
+registerlink.addEventListener('click', () => {
+  wrapper.classList.add('active');
 });
 
-loginlink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
+loginlink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
 });
 
 
